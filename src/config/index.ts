0096b051b3a77399b4207172332eb97ed3680c75
vivekpayal-dev <vivekpayal.dev@gmail.com>
2025-10-15 11:19:27 +0530

    setup node config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,25 @@
 import { config } from 'dotenv'
+import path from 'path'
+config({
+    path: path.join(__dirname, `../../.env.${process.env.NODE_ENV}`),
+})
 
-config()
+interface IConfig {
+    PORT: string
+    NODE_ENV: string
+    DB_HOST: string
+    DB_PORT: string
+    DB_USERNAME: string
+    DB_PASSWORD: string
+    DB_NAME: string
+}
 
-const { PORT, NODE_ENV } = process.env
-
-export const Config = {
-    PORT,
-    NODE_ENV,
+export const Config: IConfig = {
+    PORT: process.env.PORT!,
+    NODE_ENV: process.env.NODE_ENV!,
+    DB_HOST: process.env.DB_HOST!,
+    DB_PORT: process.env.DB_PORT!,
+    DB_USERNAME: process.env.DB_USERNAME!,
+    DB_PASSWORD: process.env.DB_PASSWORD!,
+    DB_NAME: process.env.DB_NAME!,
 }
